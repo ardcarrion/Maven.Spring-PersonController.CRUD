65e0248aa3622c63d718d1da9b44943abd290fe8
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-08-18 17:36:45 -0400

    tests

--- src/test/java/io/zipcoder/crudapp/CRUDApplicationTests.java
+++ src/test/java/io/zipcoder/crudapp/CRUDApplicationTests.java
@@ -2,11 +2,13 @@ package io.zipcoder.crudapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class CRUDApplicationTests {
 
 	@Test
